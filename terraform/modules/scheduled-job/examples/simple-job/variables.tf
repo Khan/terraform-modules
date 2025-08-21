cc031b13dbde9f5ebb519df7aa0c9a0bb4fa2c18
James Wiesebron <jameswiesebron@khanacademy.org>
2025-08-21 10:34:50 -0700

    [scheduled-job-alerts] Add alerting for failed scheduled jobs

--- terraform/modules/scheduled-job/examples/simple-job/variables.tf
+++ terraform/modules/scheduled-job/examples/simple-job/variables.tf
@@ -13,3 +13,14 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "slack_channel" {
+  description = "Slack channel to send notifications to (e.g., '#my-team-channel')"
+  type        = string
+}
+
+variable "alert_project_id" {
+  description = "GCP project ID where monitoring and alerting resources will be created (optional, defaults to project_id)"
+  type        = string
+  default     = null
+}
